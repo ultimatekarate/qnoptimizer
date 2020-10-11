f1b86ab12c83647a84c462279605a76ebc099f01
Joe Volzer <joevolzer@gmail.com>
2020-10-11 15:53:57 -0400

    Newton's method is implemented.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from qnoptimizer.tests import *
 from qnoptimizer.qnoptimizer import *
+from qnoptimizer.LineSearch import *
--- qnoptimizer/LineSearch.py
+++ qnoptimizer/LineSearch.py
@@ -1,6 +1,7 @@
+import numpy as np
+
 class LineSearch:
     # Backtracking line search with quadratic AND cubic interpolation.
-
 
     # Greek letters for variable names because math reasons.
     # This particular alpha is used when checking one of the Armijo conditions.
@@ -12,14 +13,42 @@ class LineSearch:
     # Boolean flag because I'm writing this to work with other search methods.
     search_failed = False
 
+    objective_function = None
+
+    __x0 = None
     max_iterations = 10
 
-    def __init__(self):
-        pass
+    def __init__(self,F,initial_guess,max_iterations):
+        self.__x0 = initial_guess
+        self.objective_function = F
+        self.max_iterations = max_iterations
 
-    def NewtonStep(self):
-        pass
+    def NewtonStep(self,x_c):
+        F = self.objective_function
+        print(F.Hessian(x_c))
+        return -1*np.linalg.solve(F.Hessian(x_c),F.Gradient(x_c))
 
     def Backtrack(self):
         pass
 
+    def Optimize(self,initial_guess):
+        assert self.objective_function is not None,\
+            'Define an objective function.'
+        assert isinstance(self.max_iterations,int)\
+            and self.max_iterations > 0,\
+            'Maximum number of iterations must be a positive integer.' 
+
+        F = self.objective_function
+        x_c = initial_guess
+        i = 0
+
+        while i < self.max_iterations:
+            search_direction = self.NewtonStep(x_c)
+            x_plus = x_c + search_direction
+            f_plus = F.Evaluate(x_plus)
+            print(f_plus)
+            x_c = x_plus
+            i += 1
+        
+    def SetObjectiveFunction(self,F):
+        self.objective_function = F
